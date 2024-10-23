f87913ef630527b640d505c092fff4b6725b4a9a
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-10-23 22:47:16 +0700

    Update linreg.hpp - Create loss and optimizer

--- lib/linreg/include/linreg.hpp
+++ lib/linreg/include/linreg.hpp
@@ -6,34 +6,75 @@
 
 namespace linreg
 {
+    class parameter
+    {
+    public:
+        double w = 0;
+        double b = 0;
+        double mse = 0;
+    };
+
     class model
     {
     protected:
-        struct data
+        class data
         {
+        public:
             std::vector<double> x;
             std::vector<double> y;
         };
 
-        struct  parameter
+        class optimizer
         {
-            double w, b, mse;
+        public:
+            parameter gradient_descent(std::vector<double> x, std::vector<double> y, parameter param, double alpha)
+            {
+                double dw = 0;
+                double db = 0;
+                unsigned int n = x.size();
+                unsigned int i = 0;
+
+                for (i = 0; i < n; i++)
+                {
+                    double pred = (param.w * x[i]) + param.b;
+                    double e = y[i] - pred;
+                    dw += (-2.0 / n) * e * x[i];
+                    db += (-2.0 / n) * e;
+                }
+
+                param.w -= alpha * dw;
+                param.b -= alpha * db;
+
+                return param;
+            }
         };
 
         class loss
         {
-            double gradient_descent(std::vector<double> x, std::vector<double> y, double w, double b, double alpha)
+        public:
+            parameter mse(std::vector<double> x, std::vector<double> y, parameter param)
             {
-                
+                double err = 0;
+                double e = 0;
+                double pred = 0;
+                unsigned int n = x.size();
+                unsigned int i = 0;
+
+                for (i = 0; i < n; i++)
+                {
+                    pred = (param.w * x[i]) + param.b;
+                    e = y[i] - pred;
+                    err += std::pow(e, 2);
+                }
+
+                param.mse = err / n;
+
+                return param;
             }
-        };   
+        };
 
     public:
-        void load(std::vector<double> x, std::vector<double> y);
-
-        void fit(double alpha, double epochs, unsigned char track);
-
-        void getparam();
+        parameter fit(std::vector<double> x, std::vector<double> y, parameter param, double alpha, double epochs, unsigned char track);
     };
 }
 
