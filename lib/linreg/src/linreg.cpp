5b3293f5139d969c662fb61891e7329cc7a5d760
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-10-23 22:47:32 +0700

    Update linreg.cpp - Update loss and optimizer

--- lib/linreg/src/linreg.cpp
+++ lib/linreg/src/linreg.cpp
@@ -5,15 +5,35 @@
 
 namespace linreg
 {
-    void model::load(std::vector<double> x, std::vector<double> y)
+    parameter model::fit(std::vector<double> x, std::vector<double> y, parameter param, double alpha, double epochs, unsigned char track)
     {
-        model::data data;
+        data data;
         data.x = x;
         data.y = y;
-    }
 
-    void model::fit(double alpha, double epochs, unsigned char track)
-    {
+        optimizer optim;
+        loss ls;
 
+        unsigned int i;
+        for (i = 0; i < epochs; i++)
+        {
+            param = optim.gradient_descent(data.x, data.y, param, alpha);
+            param = ls.mse(data.x, data.y, param);
+            if (track == 1)
+            {
+                std::cout << "epochs: " << i << "| " << "w: " << param.w << " " << "b: " << param.b << " " << "loss: " << param.mse << std::endl;
+            }
+            else if (track == 0)
+            {
+            }
+            else
+            {
+                std::cout << "Invalid type..." << std::endl;
+                std::cout << "Set type = 1 (track)" << std::endl;
+                track = 1;
+            }
+        }
+
+        return param;
     }
 }
