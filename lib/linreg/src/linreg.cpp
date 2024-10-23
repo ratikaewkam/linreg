b25873d4a161a984e6cfbf0d384e3ef6feb72469
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-10-23 20:48:56 +0700

    Update linreg.cpp - Create method

--- lib/linreg/src/linreg.cpp
+++ lib/linreg/src/linreg.cpp
@@ -5,5 +5,15 @@
 
 namespace linreg
 {
-    
+    void model::load(std::vector<double> x, std::vector<double> y)
+    {
+        model::data data;
+        data.x = x;
+        data.y = y;
+    }
+
+    void model::fit(double alpha, double epochs, unsigned char track)
+    {
+
+    }
 }
