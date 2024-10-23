f309e2ceeffb03eba98ec202b41ea5479d867b47
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-10-23 20:48:42 +0700

    Update linreg.hpp - Create model

--- lib/linreg/include/linreg.hpp
+++ lib/linreg/include/linreg.hpp
@@ -6,7 +6,35 @@
 
 namespace linreg
 {
-    
+    class model
+    {
+    protected:
+        struct data
+        {
+            std::vector<double> x;
+            std::vector<double> y;
+        };
+
+        struct  parameter
+        {
+            double w, b, mse;
+        };
+
+        class loss
+        {
+            double gradient_descent(std::vector<double> x, std::vector<double> y, double w, double b, double alpha)
+            {
+                
+            }
+        };   
+
+    public:
+        void load(std::vector<double> x, std::vector<double> y);
+
+        void fit(double alpha, double epochs, unsigned char track);
+
+        void getparam();
+    };
 }
 
 #endif
